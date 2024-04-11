049c8642cd702561dc73c8d5612e2faff0e452ec
L33t-Rose <jphillipjean@gmail.com>
2024-04-10 23:19:34 -0400

    Refactor process_data.py to output a csv instead of JSON

--- src/process_data.py
+++ src/process_data.py
@@ -1,26 +1,27 @@
-import json
 import os
+import csv
 
 csv_files = os.listdir('./data')
-output = []
 product_target = 'pink morsel'
-for csv_file in csv_files:
-    with open(f'./data/{csv_file}','r') as f:
-        # Read the file contents,split on \n, and truncate the first column
-        content = f.read().splitlines()[1::]
-        # Iterate through each row, split on ",", and then process the row
-        for row in content:
-            arr = row.split(',')
-            if not arr[0] == product_target:
-                continue
-            data = {
-                "Sales":float(arr[1].replace('$','')) * int(arr[2]),
-                "Date":arr[3],
-                "Region":arr[4]
-            }
-            output.append(data)
-            
-with open('./output.json','w') as f:
-    json.dump(output,f)
+# newline='' needs to be included because the writer was adding extra blank lines between each row
+# https://stackoverflow.com/questions/3348460/csv-file-written-with-python-has-blank-lines-between-each-row
+with open('output.csv','+w',newline='') as output_file:
+    writer = csv.writer(output_file,delimiter=",",)
+    writer.writerow(['sales','data','region'])
+    for csv_file in csv_files:
+        with open(f'./data/{csv_file}','r') as f:
+            # Read the file contents,split on \n, and remove the first column
+            content = f.read().splitlines()[1::]
+            # Iterate through each row, split on ",", and then process the row
+            for row in content:
+                arr = row.split(',')
+                if not arr[0] == product_target:
+                    continue
+                data = [
+                    float(arr[1].replace('$','')) * int(arr[2]),
+                    arr[3],
+                    arr[4]
+                ]
+                writer.writerow(data)
 
 
